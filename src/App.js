7ad05e392762b7b6894fb3acc363387686013c0b
szh <szh@sdkfz233deMacBook-Pro.local>
2019-11-05 16:14:18 +0900

    React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import NameLabel from "./NameLabel";
+
+function App() {
+  
+  return (
+    <div>
+    <h1> Hello !</h1>
+    <NameLabel name="Aedgen" title="react"/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/NameLabel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class NameLabel extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>this is belonging to {this.props.name}</p>
+                <p>made by {this.props.title}</p>
+            </div>
+        );
+    }
+}
+
+//export default NameLabel;
